e31f12cef3c588ddf85eab7e5656c67624e28c36
codylovell <codylovell94@gmail.com>
2018-03-26 10:08:24 -0400

    tidying up

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'y23qqf+e-=q&+&$yc$h@&2i1ub3a7^c-s0v&v3zc!$1(6%7k6i'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
