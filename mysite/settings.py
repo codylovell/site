6d34c8257200791f838e341830e28c13134d81bc
codylovell <codylovell94@gmail.com>
2018-04-08 16:07:33 -0400

    fixed link in footer

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y23qqf+e-=q&+&$yc$h@&2i1ub3a7^c-s0v&v3zc!$1(6%7k6i'
+SECRET_KEY = 'l*$h&5+c66h=ebggq=-qc*sm!b#^&g7htg57+vp3mbp7kb!$xi'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
